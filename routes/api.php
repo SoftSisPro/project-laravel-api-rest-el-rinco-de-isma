679348620c004b996ad60339de4f32c091610149
SoftSisPro <softsispro@gmail.com>
2024-10-19 19:09:09 -0500

    Creando un datos y devolver procesos

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,26 @@ Route::patch('/usuario/{name}', function ($name) {
 Route::delete('/usuario/{name}', function ($name) {
     return "Se ha borrado el usuario con el id $name";
 });
+
+//- Mi primera funcion
+Route::get('/random/{min}/{max}', function ($min, $max) {
+    //- Validar que los valores sean numericos
+    if(!is_numeric($min) || !is_numeric($max)){
+        return response()->json([
+            'error' => 'Los valores deben ser numericos'
+        ], 400);
+    }
+
+    //- Validar que el valor minimo sea menor al valor maximo
+    if($min > $max){
+        return response([
+            'error' => 'El valor minimo debe ser menor al valor maximo'
+        ], 400);
+    }
+
+    $number = rand($min, $max);
+    return response([
+        'random' => $number
+    ], 200);
+
+});
